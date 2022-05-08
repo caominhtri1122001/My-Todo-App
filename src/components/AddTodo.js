dfef4b242e81b12d1ef05132ef873d42d026e48f
Cao Minh Tri <caominhtri112201@gmail.com>
2022-05-08 16:22:01 +0700

    class component

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,34 +1,28 @@
-import React from "react";
+import React , {useState} from "react";
 
-class AddTodo extends React.Component {
-    state = {
-        title: ""
+function AddTodo (props) {
+    const [title, setTitle] = useState("");
+
+    const onInputChange = (e) => {
+        setTitle(e.target.value)
     }
-    render() {
-        return (
-            <form className="form-container" onSubmit={this.AddNewTodo}>
-                <input type='text' 
-                    placeholder="Add todo..." 
-                    className="input-text"
-                    value={this.state.title}
-                    onChange={this.onInputChange}>
-                </input>
-                <input type='submit' value="Submit" className="input-submit"></input>
-            </form>
-        );
-    }
-    onInputChange = (e) => {
-        this.setState({
-            title: e.target.value
-        })
-    }
-    AddNewTodo = (e) => {
+    const AddNewTodo = (e) => {
         e.preventDefault();
-        this.props.addTodo(this.state.title);
-        this.setState({
-            title:""
-        })
+        props.addTodo(title);
+        setTitle("");
     }
+
+    return (
+        <form className="form-container" onSubmit={AddNewTodo}>
+            <input type='text' 
+                placeholder="Add todo..." 
+                className="input-text"
+                value={title}
+                onChange={onInputChange}>
+            </input>
+            <input type='submit' value="Submit" className="input-submit"></input>
+        </form>
+    );
 }
 
 export default AddTodo
